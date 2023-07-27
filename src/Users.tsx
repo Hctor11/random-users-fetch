7d450f07968c4c413a0992605068836b46e3a00f
hector <hectorrrivera1010@gmail.com>
2023-07-27 15:21:07 -0600

    add fetch to users api

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import type { User } from "./User";
 
 const Users = () => {
-    const [Users, setUsers] = useState(Array<User>);
+    const [Users, setUsers] = useState(Array<User[]>());
+    useEffect(() => {
+      fetch('https://randomuser.me/api/?results=50')
+        .then(res => res.json())
+        .then(res => setUsers(res.results))
+        .catch(err => console.error(err))      
+    },[])
 
   return (
     <div>
         <h1>Hola users</h1>
+        {JSON.stringify(Users)}
     </div>
   )
 }
