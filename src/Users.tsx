50d913afb6a48a4c1e9a83f7dea6d4885d3b6416
hector <hectorrrivera1010@gmail.com>
2023-07-26 21:26:02 -0600

    add type User interface

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react"
+import type { User } from "./User";
 
 const Users = () => {
+    const [Users, setUsers] = useState(Array<User>);
+
   return (
     <div>
         <h1>Hola users</h1>
