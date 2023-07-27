9037395a9ce60b0c85c6cee2c85051c9c78041e6
hector <hectorrrivera1010@gmail.com>
2023-07-26 21:07:49 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Users from './Users'
+
+function App() {
+
+  return (
+    <>
+      <Users></Users>
+    </>
+  )
+}
+
+export default App
---
+++ src/Users.tsx
@@ -0,0 +1,10 @@
+
+const Users = () => {
+  return (
+    <div>
+        <h1>Hola users</h1>
+    </div>
+  )
+}
+
+export default Users
